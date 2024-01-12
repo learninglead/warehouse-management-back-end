47566b133ee41d9f5afdac523d3b13f9775a869a
Ömer Eryılmaz <omer-eryilmaz@hotmail.com>
2024-01-12 20:20:10 +0300

    Comments edited

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const { SERVER_PORT } = require('./constants');
 
-//import routes
+// import routes
 const userRoutes = require('./routes/user.route');
 
 // import middlewares
@@ -11,13 +11,13 @@ const { errorHandler } = require('./middlewares/error.middleware');
 // initialize express middleware
 app.use(express.json());
 
-//initialize routes
+// initialize routes
 app.use('/api', userRoutes);
 
 // error middleware
 app.use(errorHandler);
 
-//app start
+// app start
 const appStart = () => {
     try {
         app.listen(SERVER_PORT, () => {
